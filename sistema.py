608e8b1bc8ed173b87c71d0780659a02f8d9f6b4
Daniela Gomes <danielasgs08@gmail.com>
2022-08-23 11:55:54 -0300

    Initial commit

---
+++ sistema.py
@@ -0,0 +1,26 @@
+from sistemaModularizado.lib.interface import *
+from sistemaModularizado.lib.arquivo import *
+from time import sleep
+
+arq = 'CursoEmVideo.txt'
+
+while True:
+    cabecalho('MENU PRINCIPAL')
+    resposta = menu(['Criar arquivo', 'Cadastrar pessoa', 'Listar pessoas', 'Sair do sistema'])
+    if resposta == 1:
+        if not arquivoExiste(arq):
+            criarArquivo(arq)
+    elif resposta == 2:
+        cabecalho('NOVO CADASTRO')
+        nome = str(input('NOME: '))
+        idade = leiaInt('IDADE: ')
+        cadastrarPessoa(arq, nome, idade)
+    elif resposta == 3:
+        lerArquivo(arq)
+
+    elif resposta == 4:
+        print('Saindo do sistema... Até logo!')
+        break
+    else:
+        print('\033[31mERRO! Insira uma opção válida!\033[m')
+    sleep(2)
